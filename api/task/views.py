fab5ee92155bca75e7a2c0f858b08c97fb8e3a37
kharis12-web <hatami391998@gmail.com>
2020-10-05 17:20:28 +0700

    belajar Http Respone dan JSON respone

--- api/task/views.py
+++ api/task/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Crud
 
-# Create your views here.
+
+# untuk HttpResponse 
+
+# def index (req):
+#     html = "apa aja bileh"
+#     return HttpResponse(html)
+
+
+# untuk JsonResponse
+
+def index(req):
+    data = Crud.objects.all() #query set
+
+    simpan = [] #ibarat kaleng kosong untuk menyimpan data
+    for e in data: #query list
+        simpan.append({
+            'nama':e.nama, # append untuk menambahkan data
+            'motto':e.motto
+        })
+    return JsonResponse({
+        'data': simpan #
+    }, safe=False)
